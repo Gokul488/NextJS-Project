ae8f87a605db81010e648a59844350b4b3569bf9
Gokul488 <gokul042000@gmail.com>
2026-04-22 15:44:09 +0530

    file upload system added2

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { EdgeStoreProvider } from "@/lib/edgestore-client";
+import { EdgeStoreProvider } from "@/lib/edgestore-client";  // ← fix this line
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <EdgeStoreProvider>{children}</EdgeStoreProvider>;
